88bc4c4edf12dfa97e98e73ef269557f738339b2
arthurolivierfortin <aofor@ulaval.ca>
2022-11-25 22:13:25 -0500

    début

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+def bob_it:(difficulté):
+    def jouer_partie:
+        x = random.randint(1,4)
+        print(x)
+        y = input(Pèse sur le bouton)
+        if y == x:
+        bob

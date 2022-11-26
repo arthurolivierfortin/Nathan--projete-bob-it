9db8ebe3c5975b127158c85e693c1a251cb278ce
arthurolivierfortin <aofor@ulaval.ca>
2022-11-26 09:51:03 -0500

    version 1.2

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
 import random
 import time
-def bob_it(difficulté):
-    x = random.randint(1,10)
-    print(x)
-    print(time)
-    y = int(input('pèse sur le bouton'))
-    if y == x:
-        bob_it(difficulté)
-    if y != x:
-        return'tes pourris'
+from threading import *
+
+
+class bob_it(Thread):
+    def run(difficulté):
+        x = random.randint(1,10)
+        print(f'==> {x}')
+        y = int(input('pèse sur le bouton'))
+        if y == x:
+            bob_it(difficulté)
+        if y != x:
+            print('tes pourris')
+
+
+class timer(Thread):
+    def run(self):
+        difficulté = int(input('choisi ta difficulté (1,2,3)'))
+        sec = 3
+        for i in range(sec):
+            if difficulté == 3:
+                print(f'{sec}')
+                time.sleep(0.5)
+                sec -= 1 
+                if sec == 0:
+                    print('game over')
+                    break
+            if difficulté == 2:
+                print(f'{sec}')
+                time.sleep(1)
+                sec -= 1 
+                if sec == 0:
+                    print('game over')
+                    break
+            if difficulté == 1:
+                print(f'{sec}')
+                time.sleep(1.5)
+                sec -= 1 
+                if sec == 0:
+                    print('game over')
+                    break
+fct_timer = timer()
+fct_bob_it = bob_it()
     
-bob_it(1)
+fct_timer.start()
+fct_bob_it.start()

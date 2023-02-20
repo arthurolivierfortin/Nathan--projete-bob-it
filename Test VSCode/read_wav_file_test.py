81b5f660aefb931d1fa5b779e92b75d7c07982a6
Arthur-Olivier Fortin <aofor@ulaval.ca>
2023-02-20 17:35:06 -0500

    lire sons fait

--- Test VSCode/read_wav_file_test.py
+++ Test VSCode/read_wav_file_test.py
@@ -19,13 +19,25 @@ def read_wav_file(filename):
         #for i in range(len(audio_data)-1):
         #    un_point += [audio_data[i][0]]
         #print(np.iinfo(np.int8).max)
-        print(np.iinfo(np.int8).min)
+        #print(np.iinfo(np.int8).min)
         
         file = open('1point.txt', 'w')
+        
+        #x = (((audio_data[len(audio_data)-1-1][0])+128)*256)
+        #print(x)
+        #print(len(str(x)))
+        #y = int(5-len(str(x)))
+        #print("0"*y+str(x))
+        #print(len(x))
         for i in range(len(audio_data)-1):
             #print((audio_data_norme[len(audio_data_norme)-i-1][0])
             #print(int((audio_data[len(audio_data)-i-1][0])+128)*256)
-            file.write(f"{(((audio_data[len(audio_data)-i-1][0])+128)*256)}")
+            x = (((audio_data[len(audio_data)-i-1][0])+128)*256)
+            print(x)
+            print(f"{len(str(x))} = len de x")
+            y = int(5-len(str(x)))
+            print("0"*y+str(x))
+            file.writelines("0"*y+str(x))
         file.close()
         #print(audio_data / np.iinfo(np.int16).max)
         return audio_data
--- Version pour raspberry pi/test.py
+++ Version pour raspberry pi/test.py
@@ -1,5 +1,17 @@
+import linecache
+
 onepoint = open('1point.txt', 'r')
 #for i in (1, 3777):
 
 #    print(int(onepoint.readline(i)))
-print(int(onepoint.read(111)))
+x = (onepoint.readlines())
+#print(x[0][0:5])
+byt_debut = 0
+byt_fin = 5
+for i in range(3776):
+            #speaker.duty_u16(int(print(x[0][byt_debut:byt_fin])))
+    print(int(x[0][byt_debut:byt_fin]))
+    byt_debut += 5
+    byt_fin += 5
+
+

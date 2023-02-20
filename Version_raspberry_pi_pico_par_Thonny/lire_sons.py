4d81e7679db1a4a30daa9273f69e845bf0c947b2
arthurolivierfortin <118319678+arthurolivierfortin@users.noreply.github.com>
2023-02-20 17:37:44 -0500

    Create lire_sons.py

---
+++ Version_raspberry_pi_pico_par_Thonny/lire_sons.py
@@ -0,0 +1,16 @@
+speaker = PWM(machine.Pin(27))
+speaker.freq(1000)
+
+
+def readSound(sons):
+    if sons == "1point":
+        byt_debut = 0
+        byt_fin = 5
+        onepoint = open('1point.txt', 'r')
+        x = (onepoint.readlines())
+
+        for i in range(3777):
+            speaker.duty_u16(int(print(x[0][byt_debut:byt_fin])))
+            #print(int(x[0][byt_debut:byt_fin]))
+            byt_debut += 5
+            byt_fin += 5

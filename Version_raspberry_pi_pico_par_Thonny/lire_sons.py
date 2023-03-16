a061572136989d21185d19e2d5acdb4f9d1345f1
arthurolivierfortin <118319678+arthurolivierfortin@users.noreply.github.com>
2023-03-16 18:37:20 -0400

    Update lire_sons.py

--- Version_raspberry_pi_pico_par_Thonny/lire_sons.py
+++ Version_raspberry_pi_pico_par_Thonny/lire_sons.py
@@ -10,7 +10,6 @@ def readSound(sons):
         x = (onepoint.readlines())
 
         for i in range(3777):
-            speaker.duty_u16(int(print(x[0][byt_debut:byt_fin])))
+            speaker.duty_u16(int(print(x[i][0:5])))
             #print(int(x[0][byt_debut:byt_fin]))
-            byt_debut += 5
-            byt_fin += 5
+            
